72bd7e20dec132d93606a7f90f26c41cdd905c59
Francesco Marrocco <frmarrocco03@gmail.com>
2026-07-16 15:07:41 +0200

    Add Firebase browser client

---
+++ firebase-client.js
@@ -0,0 +1,47 @@
+import {
+  ADMINS_COLLECTION,
+  FIREBASE_CONFIG,
+  FIREBASE_SDK_VERSION,
+  SETTINGS_COLLECTION,
+  SETTINGS_DOCUMENT,
+  isFirebaseConfigured,
+} from "./firebase-config.js";
+
+let corePromise;
+let authPromise;
+
+function sdkUrl(service) {
+  return `https://www.gstatic.com/firebasejs/${FIREBASE_SDK_VERSION}/firebase-${service}.js`;
+}
+
+async function loadCore() {
+  if (!isFirebaseConfigured()) throw new Error("Firebase non configurato in firebase-config.js.");
+  if (!corePromise) {
+    corePromise = Promise.all([
+      import(sdkUrl("app")),
+      import(sdkUrl("firestore")),
+    ]).then(([appApi, firestoreApi]) => {
+      const app = appApi.getApps().length ? appApi.getApp() : appApi.initializeApp(FIREBASE_CONFIG);
+      return { app, db: firestoreApi.getFirestore(app), appApi, firestoreApi };
+    });
+  }
+  return corePromise;
+}
+
+async function loadAuth(app) {
+  if (!authPromise) {
+    authPromise = import(sdkUrl("auth")).then((authApi) => ({ authApi, auth: authApi.getAuth(app) }));
+  }
+  return authPromise;
+}
+
+export async function getFirebaseServices({ includeAuth = false } = {}) {
+  const core = await loadCore();
+  return includeAuth ? { ...core, ...(await loadAuth(core.app)) } : core;
+}
+
+export const firebasePaths = Object.freeze({
+  adminsCollection: ADMINS_COLLECTION,
+  settingsCollection: SETTINGS_COLLECTION,
+  settingsDocument: SETTINGS_DOCUMENT,
+});
